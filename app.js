7cfb1c4ecfceadece41ade9ecb9b6e70f239af85
Hamed Noroozi <noruzi206@gmail.com>
2024-02-20 23:57:36 +0330

    show the data of weather in ui

--- app.js
+++ app.js
@@ -4,6 +4,7 @@ const BASE_URL = `https://api.openweathermap.org/data/2.5`;
 
 const searchInput = document.querySelector("input")
 const searchButton = document.querySelector("button")
+const weatherContainer = document.getElementById("weather")
 
 const getCurrentWeatherByName = async (city) => {
    const url = `${BASE_URL}/weather?q=${city}&units=metric&appid=${API_KEY}`;
@@ -11,6 +12,23 @@ const getCurrentWeatherByName = async (city) => {
    const json = await res.json();
    return json;
 }
+
+const renderCurrentWeather = data => {
+   const weatherJSX = `
+      <h1>${data.name},${data.sys.country}</h1>
+      <div id="main">
+         <img src="http://openweathermap.org/img/w/${data.weather[0].icon}.png"/>
+         <span>${data.weather[0].main}</span>
+         <p>${Math.round(data.main.temp)} °C</p>
+      </div>
+      <div id="info">
+      <p>Humidity: <span>${data.main.humidity} %</span></p>
+      <p>Wind Speed: <span>${data.wind.speed} m/s</span></p>
+      </div>
+   `;
+   weatherContainer.innerHTML = weatherJSX;
+}
+
 const searchHandler = async () => {
    const cityName = searchInput.value;
 
@@ -18,8 +36,7 @@ const searchHandler = async () => {
       alert("Please enter city name!")
    }
    const currentData = await getCurrentWeatherByName(cityName);
-   console.log(currentData);
-
+   renderCurrentWeather(currentData);
 }
 searchButton.addEventListener("click", searchHandler);
 
